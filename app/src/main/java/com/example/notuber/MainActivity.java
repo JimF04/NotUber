c738b0a480c8b2cecce0457efcc24858a3fede26
JimF04 <133442205+JimF04@users.noreply.github.com>
2023-11-03 20:07:25 -0600

    Se agregó ventanas para logueo

--- app/src/main/java/com/example/notuber/MainActivity.java
+++ app/src/main/java/com/example/notuber/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.notuber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mDriver, mEmployee;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDriver = (Button) findViewById(R.id.btnDriver);
+        mEmployee = (Button) findViewById(R.id.btnEmployee);
+
+        mDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        mEmployee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EmployeeLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
     }
 }
