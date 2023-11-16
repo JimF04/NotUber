a24241bbe91823f543dfdb7d0e339caa6f163702
Samuel Mar?n Soto <samarin@estudiantec.cr>
2023-11-16 17:55:03 -0600

    inicio de funcionalidad de amigos

--- app/src/main/java/com/example/notuber/ui/Driver/friends/FriendsFragment_Driver.java
+++ app/src/main/java/com/example/notuber/ui/Driver/friends/FriendsFragment_Driver.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import com.google.android.material.textfield.TextInputEditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.notuber.databinding.FragmentFriendsBinding;
+import java.util.List;
 
 public class FriendsFragment_Driver extends Fragment {
 
@@ -24,8 +29,27 @@ public class FriendsFragment_Driver extends Fragment {
         binding = FragmentFriendsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textFriends;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        TextInputEditText correoamigo = binding.correoamigo;
+        Button buttonAddFriend = binding.Button;
+        LinearLayout linearLayout = binding.linearLayout;
+
+        buttonAddFriend.setOnClickListener(v -> {
+            String friendName = correoamigo.getText().toString();
+            // request de API REST que agregue amigo va aqui
+            /* agarrar lista de amigos de servidor con otro request
+                procesar y meterla en variable lista
+             */
+            List<Integer> amigos = List.of(1, 2, 3, 4, 5);
+            linearLayout.removeAllViews(); // Clear existing views
+
+            for (Integer number : amigos) {
+                TextView textView = new TextView(getContext());
+                textView.setText("Section " + number);
+                textView.setPadding(10, 10, 10, 10);
+                linearLayout.addView(textView);
+            }
+        });
+
         return root;
     }
 
